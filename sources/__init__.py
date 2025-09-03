7966f7e372d7ef2a9bb8898996ff6f2af89ceb6b
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2025-09-03 12:14:39 +0200

    Hide errors

--- sources/__init__.py
+++ sources/__init__.py
@@ -10,8 +10,11 @@ RELEASE_RETRIEVER: dict[str, Callable[[str, datetime], Optional[Notification]]] 
 def InputSource(cls: type):
     typeName = cls.__name__.upper()
 
+    #pyrefly: ignore missing-attribute
     URL_VALIDATOR[typeName] = cls.is_url_valid
+    #pyrefly: ignore missing-attribute
     URL_NAME_EXTRACTOR[typeName] = cls.get_name
+    #pyrefly: ignore missing-attribute
     RELEASE_RETRIEVER[typeName] = cls.get_release
 
 from .github import Github
